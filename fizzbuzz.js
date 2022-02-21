c532ea8334521ff39c791593bd9dd4e66b3aff84
Felipe de Oliveira Teixeira <86241975+Dralker@users.noreply.github.com>
2022-02-21 20:34:12 -0300

    Add files via upload

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,21 @@
 //fizzBuzz
 //divisivel por 3 => Fizz
 //divisivel por 5 => Buzz
-//divisivel por 15 => FizzBuzz
+//divisivel por 3 e 5 => FizzBuzz
 // entrada diferente de numero => não é um numero
 
-const resultados = fizzBuzz(true);
+const resultados = fizzBuzz('luisa');
 console.log(resultados);
 
 function fizzBuzz(entrada){
     if(typeof entrada !== 'number')
         return 'não é um numero';
+    if((entrada % 3 === 0) && (entrada %5==0))
+        return 'FizzBuzz';
     if(entrada % 3 === 0)
         return 'Fizz';
     if(entrada % 5 === 0)
         return 'Buzz';
-    if((entrada % 3 === 0) && (entrada %5==0))
-    return 'FizzBuzz';
     else
         return entrada;
 };
